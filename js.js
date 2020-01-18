9f912c84afa0a807555d725d50a669101e80444b
unknown <solracsotnas003@gmail.com>
2020-01-18 12:18:40 +0800

    ito yung script

---
+++ js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn-table").click(function(){
+        $("#btn-container").load("lamesa.html");
+    });
+});
+
+$(document).ready(function(){
+    $("#btn-dialog").click(function(){
+        $("#btn-container").load("dialog.Html.html");
+    })
+})
